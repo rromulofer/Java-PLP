c99b646c257844c5f24f1467be19d6585d95b04c
Rômulo Souza Fernandes <00119110559@pq.uenf.br>
2022-10-18 16:35:53 -0300

    update

---
+++ ex1a.java
@@ -0,0 +1,17 @@
+//Como criar objeto 
+Aluno a1 = new  Aluno(); //criando um objeto de Aluno  
+
+
+
+
+//Como criar classe
+<modificador de acesso> class Nomedaclasse {
+
+    // Local onde atributos, construtores e métodos são criados
+  
+  }
+
+  public class Teste{
+    //ATRIBUTOS OU PROPRIEDADES
+    //MÉTODOS
+    }

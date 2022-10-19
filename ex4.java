eac49935ae664f6737b8f0b1489c71209ce5693a
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-19 13:34:04 -0300

    update

--- ex4.java
+++ ex4.java
@@ -1,26 +1,29 @@
 /*
 UENF - CCT - CC
+Paradigmas de Linguagens de Programação
 Aluno: Rômulo Souza Fernandes
+E-mail: 00119110559@pq.uenf.br
 Data de criação: 19/10/22 
 */
 package exercicioPLP;
+
 import java.util.Scanner;
 
 public class Exercicio4 {
 
-	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-	
-	System.out.print("Digite um número número inteiro: ");
-	int numero1 = input.nextInt();
-	
-	System.out.print("Digite outro número inteiro: ");
-	int numero2 = input.nextInt();
-	
-	System.out.printf("\nA soma entre %d com %d é de: %d",numero1,numero2,numero1+numero2);
-	System.out.printf("\nA subtração entre %d com %d é de: %d",numero1,numero2,numero1-numero2);
-	System.out.printf("\nA multiplicação entre %d com %d é de: %d",numero1,numero2,numero1*numero2);
-	System.out.printf("\nA divisão entre %d com %d é de: %d",numero1,numero2,numero1/numero2);
-	}
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite um número número inteiro: ");
+        int numero1 = input.nextInt();
+
+        System.out.print("Digite outro número inteiro: ");
+        int numero2 = input.nextInt();
+
+        System.out.printf("\nA soma entre %d com %d é de: %d", numero1, numero2, numero1 + numero2);
+        System.out.printf("\nA subtração entre %d com %d é de: %d", numero1, numero2, numero1 - numero2);
+        System.out.printf("\nA multiplicação entre %d com %d é de: %d", numero1, numero2, numero1 * numero2);
+        System.out.printf("\nA divisão entre %d com %d é de: %d", numero1, numero2, numero1 / numero2);
+    }
 
 }
--- ex6.java
+++ ex6.java
@@ -1,3 +1,24 @@
-public class ex6 {
-    
+/*
+UENF - CCT - CC
+Paradigmas de Linguagens de Programação
+Aluno: Rômulo Souza Fernandes
+E-mail: 00119110559@pq.uenf.br
+Data de criação: 19/10/22 
+*/
+package exercicioPLP;
+
+public class Exercicio6 {
+
+	public static void main(String[] args) {
+
+		int a = 5;
+		int b = 40;
+		int soma = 0;
+
+		for (int i = 5; i <= b; i++) {
+			soma += i;
+		}
+		System.out.printf("O valor da soma entre %d a %d foi de: %d", a, b, soma);
+	}
+
 }

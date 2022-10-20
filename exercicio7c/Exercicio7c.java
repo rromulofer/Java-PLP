f519525a926e682bd9abb624378bf09fc08286b4
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-20 01:50:27 -0300

    update

--- exercicio7c/Exercicio7c.java
+++ exercicio7c/Exercicio7c.java
@@ -1,5 +1,17 @@
-package exercicio7c;
+*/
+package exercicioPLP;
 
 public class Exercicio7c {
-    
+
+	public static void main(String[] args) {
+		
+		PeixeBoi cadastro = new PeixeBoi();
+		cadastro.identificacao = 001;
+		cadastro.localDeNascimento = "Campos dos Goytacazes";
+		cadastro.peso = 12;
+		
+		cadastro.verificaPeso();
+		
+	}
+
 }

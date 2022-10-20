a86bf1a5d7f7c8c28e3c98fdbb4adcfc58a05070
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-20 01:57:44 -0300

    update

--- exercicio9b/Exercicio9b.java
+++ exercicio9b/Exercicio9b.java
@@ -1,5 +1,39 @@
-package exercicio9b;
+package exercicioPLP;
+
+import java.util.Scanner;
 
 public class Exercicio9b {
-    
+
+    public static void main(String[] args) {
+        LinearEquation obj = new LinearEquation();
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Digite o valor para 'a': ");
+        obj.setA(input.nextInt());
+
+        System.out.print("Digite o valor para 'b': ");
+        obj.setB(input.nextInt());
+
+        System.out.print("Digite o valor para 'c': ");
+        obj.setC(input.nextInt());
+
+        System.out.print("Digite o valor para 'd': ");
+        obj.setD(input.nextInt());
+
+        System.out.print("Digite o valor para 'e': ");
+        obj.setE(input.nextInt());
+
+        System.out.print("Digite o valor para 'f': ");
+        obj.setF(input.nextInt());
+
+        boolean result1 = obj.isSolvable();
+        System.out.printf("\nisSolvable: %b\n", result1);
+
+        double result2 = obj.getX();
+        System.out.printf("X: %.2f\n", result2);
+
+        double result3 = obj.getY();
+        System.out.printf("Y: %.2f\n", result3);
+    }
+
 }

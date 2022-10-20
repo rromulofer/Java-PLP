a49b3ed9c2bd24832a0f6b47c4b9ace35437177e
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-20 01:52:18 -0300

    update

--- exercicio7d/Computador.java
+++ exercicio7d/Computador.java
@@ -1,5 +1,19 @@
-package exercicio7d;
+package exercicioPLP;
 
 public class Computador {
-    
+
+	String processador;
+	int quantidadeMemoriaRam;
+	
+	Computador(String processador){
+		this.processador =processador;
+	}
+	
+	public void verificaRequisitos() {
+		if (quantidadeMemoriaRam < 8) {
+			System.out.println("O computador não possui os requisitos mínimos para executar o programa. ");
+		} else {
+			System.out.println("Seu computador possui os requisitos mínimos para executar o programa. ");
+		}
+	}
 }

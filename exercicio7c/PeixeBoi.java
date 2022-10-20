f059b72473e560dc468aa3ea5e3a5a7cd6c41467
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-20 01:51:22 -0300

    update

--- exercicio7c/PeixeBoi.java
+++ exercicio7c/PeixeBoi.java
@@ -2,14 +2,14 @@ package exercicioPLP;
 
 public class PeixeBoi extends Mamifero {
 
-	double peso;
-	int identificacao;
-	
-	public void verificaPeso() {
-		if(peso<7) {
-			System.out.println("Abaixo do peso. ");
-		}else {
-			System.out.println("Dentro do peso ideal. ");
-		}
-	}
+    double peso;
+    int identificacao;
+
+    public void verificaPeso() {
+        if (peso < 7) {
+            System.out.println("Abaixo do peso. ");
+        } else {
+            System.out.println("Dentro do peso ideal. ");
+        }
+    }
 }

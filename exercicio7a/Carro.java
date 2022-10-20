992b2576fe9b49d693eb2bf77802f7d0124cef9f
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-20 02:02:09 -0300

    update

--- exercicio7a/Carro.java
+++ exercicio7a/Carro.java
@@ -3,7 +3,7 @@ UENF - CCT - CC
 Paradigmas de Linguagens de Programação
 Aluno: Rômulo Souza Fernandes
 E-mail: 00119110559@pq.uenf.br
-Data de criação: 19/10/22 
+Data de criação: 20/10/22 
 */
 package exercicioPLP;
 
--- exercicio7a/Exercicio7a.java
+++ exercicio7a/Exercicio7a.java
@@ -1,3 +1,9 @@
+/*
+UENF - CCT - CC
+Paradigmas de Linguagens de Programação
+Aluno: Rômulo Souza Fernandes
+E-mail: 00119110559@pq.uenf.br
+Data de criação: 20/10/22 
 */
 package exercicioPLP;
 
@@ -6,25 +12,25 @@ import java.util.Scanner;
 public class Exercicio7a {
 
 	public static void main(String[] args) {
-		
+
 		Scanner input = new Scanner(System.in);
 		Utilitario frota1 = new Utilitario(2017);
-	
+
 		System.out.print("Digite a marca do veículo: ");
 		frota1.marca = input.nextLine();
-		
+
 		System.out.print("Digite o modelo do veículo: ");
 		frota1.modelo = input.nextLine();
-		
+
 		System.out.print("Digite a quilometragem percorrida: ");
 		frota1.quilometragem = input.nextDouble();
-		
+
 		System.out.print("Digite a quantidade de combustível abastecida: ");
 		frota1.litragemCombustivel = input.nextDouble();
-		
+
 		frota1.consumo();
 		frota1.verificaIPVA();
-		
+
 	}
 
 }

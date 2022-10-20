548b5c00ecd5890d659d41bb388b66d4312cfb79
Rômulo Souza Fernandes <rromulofer@gmail.com>
2022-10-19 22:56:02 -0300

    update

--- exercicio7a/Utilitario.java
+++ exercicio7a/Utilitario.java
@@ -1,12 +1,12 @@
 package exercicioPLP;
 
-public class Utilitario extends Carro{
-	
-	Utilitario(int anoDeFabricacao) {
-		super(anoDeFabricacao);
-	}
-	
-	String marca;
-	String modelo;
-	
+public class Utilitario extends Carro {
+
+    Utilitario(int anoDeFabricacao) {
+        super(anoDeFabricacao);
+    }
+
+    String marca;
+    String modelo;
+
 }
--- exercicio7b/Exercicio7b.java
+++ exercicio7b/Exercicio7b.java
@@ -1,5 +1,14 @@
-package exercicio7b;
+package exercicioPLP;
 
 public class Exercicio7b {
-    
+
+	public static void main(String[] args) {
+
+		Tomate cadastro = new Tomate(1);
+		cadastro.citrico = false;
+		
+		
+		cadastro.verificaValidade();
+	}
+
 }
